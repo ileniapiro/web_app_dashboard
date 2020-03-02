d69560fe0ab294e6a8774f2c793970defa577a0e
Ilenia Piro <ilenia.piro@gmail.com>
2020-03-02 13:45:48 +0100

    Added notification dropdown

---
+++ js/notification.js
@@ -0,0 +1,22 @@
+document.getElementById("dropbutton").addEventListener("click", () => {
+  document.getElementById("myDropdown").classList.toggle("show");
+  $('#dot').css({
+        'opacity': 0
+    });
+})
+
+/*
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+*/
